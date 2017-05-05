4bbbb034185a7e823d8121f09869842b94bd7e7a
Rieljun Liguid <me@iamriel.com>
2017-05-06 00:14:57 +0800

    Add angular and neotree

---
+++ .emacs.d/lisp/init-angular.el
@@ -0,0 +1,28 @@
+;;; init-angular.el -- Angular configuration
+;;; Commentary:
+;;
+;; It's just a matter of code.
+;;
+;;; Code:
+
+(require 'ng2-mode)
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; formats the buffer before saving
+(add-hook 'before-save-hook 'tide-format-before-save)
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+(provide 'init-angular)
+;;; init-angular.el ends here
---
+++ .emacs.d/lisp/init-neotree.el
@@ -0,0 +1,39 @@
+;;; init-neotree.el -- My neotree configuration.
+;;; Commentary:
+;;; Code:
+(use-package all-the-icons :ensure t)
+
+(use-package neotree
+  :ensure t
+  :config
+  (setq neo-theme (if (display-graphic-p) 'icons 'arrow))
+  (add-hook 'neotree-mode-hook
+    (lambda ()
+      (define-key evil-normal-state-local-map (kbd "q") 'neotree-hide)
+      (define-key evil-normal-state-local-map (kbd "I") 'neotree-hidden-file-toggle)
+      (define-key evil-normal-state-local-map (kbd "z") 'neotree-stretch-toggle)
+      (define-key evil-normal-state-local-map (kbd "R") 'neotree-refresh)
+      (define-key evil-normal-state-local-map (kbd "m") 'neotree-rename-node)
+      (define-key evil-normal-state-local-map (kbd "c") 'neotree-create-node)
+      (define-key evil-normal-state-local-map (kbd "d") 'neotree-delete-node)
+
+      (define-key evil-normal-state-local-map (kbd "s") 'neotree-enter-vertical-split)
+      (define-key evil-normal-state-local-map (kbd "S") 'neotree-enter-horizontal-split)
+
+      (define-key evil-normal-state-local-map (kbd "RET") 'neotree-enter))))
+
+(use-package find-file-in-project :ensure t)
+(defun neotree-project-dir ()
+  "Open NeoTree using the git root."
+  (interactive)
+  (let ((project-dir (ffip-project-root))
+        (file-name (buffer-file-name)))
+    (if project-dir
+        (progn
+        (neotree-dir project-dir)
+        (neotree-find file-name))
+    (message "Could not find git project root."))))
+
+(provide 'init-neotree)
+;;; init-neotree.el ends here
+
